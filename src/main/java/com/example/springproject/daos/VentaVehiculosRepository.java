54d2be85010fffd40059d90c77d797e2f9a6acf6
Javier Ladrón <jladron@deloitte.es>
2020-03-14 13:01:42 +0100

    Explicación estructura spring (controllers, service, daos)
    Inyeccion de dependencias con anotaciones

--- src/main/java/com/example/springproject/daos/VentaVehiculosRepository.java
+++ src/main/java/com/example/springproject/daos/VentaVehiculosRepository.java
@@ -5,6 +5,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Component;
+
+import com.example.springproject.model.CocheModel;
+import com.example.springproject.model.MotoModel;
 import com.example.springproject.model.VehiculoModel;
 
 /**
@@ -12,20 +16,43 @@ import com.example.springproject.model.VehiculoModel;
  * @author jladron
  *
  */
+
+@Component
 public class VentaVehiculosRepository {
 	
 	private Map<String,VehiculoModel> garaje=new HashMap<String,VehiculoModel>();
 
-	private static VentaVehiculosRepository instance=null;
 	
-	private VentaVehiculosRepository() {};
+	private VentaVehiculosRepository() {
+		CocheModel coche=new CocheModel();
+		coche.setAnnioFab(Integer.valueOf(2012));
+		coche.setDescripcion("Coche diesel gama baja de Volkswagen");
+		coche.setModelo("Volkswagen Polo");
+		coche.setMatricula("4538GYT");
+		coche.setPeso(Double.valueOf(1450));
+		coche.setModoAlquiler("basico");
+		coche.setTipo("diesel");
+		insertaNuevo(coche);
+		coche=new CocheModel();
+		coche.setAnnioFab(Integer.valueOf(2010));
+		coche.setDescripcion("Coche gasolina de peugeot");
+		coche.setModelo("Peugeout 407");
+		coche.setMatricula("8831HUQ");
+		coche.setPeso(Double.valueOf(975));
+		coche.setModoAlquiler("todo riesgo");
+		coche.setTipo("gasolina");
+		insertaNuevo(coche);
+		MotoModel moto=new MotoModel();
+		moto.setAnnioFab(Integer.valueOf(2015));
+		moto.setDescripcion("Moto eléctrica de BMW");
+		moto.setMatricula("1399AOT");
+		moto.setModelo("BMW 360T");
+		moto.setModoAlquiler("todo riesgo");
+		moto.setPeso(Double.valueOf(159));
+		insertaNuevo(moto);
+		
+	};
 	
-	public static VentaVehiculosRepository getInstance() {
-		if(instance==null) {
-			instance=new VentaVehiculosRepository();
-		}
-		return instance;
-	}
 	
 	public void insertaNuevo(VehiculoModel vehiculo) {
 		garaje.put(vehiculo.getMatricula(), vehiculo);
--- src/main/java/com/example/springproject/services/ServiceRentaVehiculosImpl.java
+++ src/main/java/com/example/springproject/services/ServiceRentaVehiculosImpl.java
@@ -2,13 +2,12 @@ package com.example.springproject.services;
 
 import java.util.List;
 
-import org.apache.commons.collections.CollectionUtils;
+
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.springproject.daos.VentaVehiculosRepository;
-import com.example.springproject.model.CocheModel;
-import com.example.springproject.model.MotoModel;
 import com.example.springproject.model.VehiculoModel;
 
 /**
@@ -19,55 +18,18 @@ import com.example.springproject.model.VehiculoModel;
 @Service
 public class ServiceRentaVehiculosImpl implements IServiceRentaVehiculos{
 	
-	VentaVehiculosRepository repoVehiculos=null;
-	
 	/**
-	 * Constructor inicial para cargar ejemplos de vehiculos
+	 * Inyectamos DAO, nuestro repo
 	 */
-	private ServiceRentaVehiculosImpl() {
-		
-		repoVehiculos=VentaVehiculosRepository.getInstance();
-		
-		List<VehiculoModel> listadoFlota=repoVehiculos.dameTodos();
-		
-		if(CollectionUtils.isEmpty(listadoFlota)) {
-			CocheModel coche=new CocheModel();
-			coche.setAnnioFab(Integer.valueOf(2012));
-			coche.setDescripcion("Coche diesel gama baja de Volkswagen");
-			coche.setModelo("Volkswagen Polo");
-			coche.setMatricula("4538GYT");
-			coche.setPeso(Double.valueOf(1450));
-			coche.setModoAlquiler("basico");
-			coche.setTipo("diesel");
-			repoVehiculos.insertaNuevo(coche);
-			coche=new CocheModel();
-			coche.setAnnioFab(Integer.valueOf(2010));
-			coche.setDescripcion("Coche gasolina de peugeot");
-			coche.setModelo("Peugeout 407");
-			coche.setMatricula("8831HUQ");
-			coche.setPeso(Double.valueOf(975));
-			coche.setModoAlquiler("todo riesgo");
-			coche.setTipo("gasolina");
-			repoVehiculos.insertaNuevo(coche);
-			MotoModel moto=new MotoModel();
-			moto.setAnnioFab(Integer.valueOf(2015));
-			moto.setDescripcion("Moto eléctrica de BMW");
-			moto.setMatricula("1399AOT");
-			moto.setModelo("BMW 360T");
-			moto.setModoAlquiler("todo riesgo");
-			moto.setPeso(Double.valueOf(159));
-			repoVehiculos.insertaNuevo(moto);
-			
-			
-		}
-	}
+	@Autowired
+	VentaVehiculosRepository repoVehiculos;
 	
 	/**
 	 * Devuelve vehiculo que coindice con la matricula pasada
 	 */
 	@Override
 	public VehiculoModel getVehiculoByMatricula(String matricula) {
-		repoVehiculos=VentaVehiculosRepository.getInstance();
+		
 		if(StringUtils.isNotBlank(matricula)) {
 			return repoVehiculos.dameVehiculo(matricula);
 		}
@@ -79,7 +41,7 @@ public class ServiceRentaVehiculosImpl implements IServiceRentaVehiculos{
 	 */
 	@Override
 	public List<VehiculoModel> getAllVehiculos() {
-		repoVehiculos=VentaVehiculosRepository.getInstance();
+		
 		return repoVehiculos.dameTodos();
 	}
 	
